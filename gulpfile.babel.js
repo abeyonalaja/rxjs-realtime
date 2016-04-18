966d7686a66746270b4a6ac6ae83b1de44a563a1
Abey Onalaja <abey.onalaja@gmail.com>
2016-04-18 23:57:53 +0100

    Add dev and prod server build settings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,8 +8,37 @@ import {create as createServerConfig} from "./webpack.server";
 const $ = require("gulp-load-plugins")();
 
 
-// ----------------
+// -----------------------------
 // Public Tasks
 gulp.task("clean:server", cb => rimraf("./build", cb));
 gulp.task("clean:client", cb => rimraf("./public/build", cb));
 gulp.task("clean", ["clean:server", "clean:client"]);
+
+gulp.task("dev:server", ["clean:server"], devServerBuild);
+
+gulp.task("prod:server", ["clean:server"], prodServerBuild);
+
+// -----------------------------
+// Private Server tasks
+const devServerWebpack = webpack(createServerConfig(true));
+
+
+function devServerBuild(callback){
+    devServerWebpack.run((error, stats) => {
+        outputWebpack("Dev:Server", error, stats);
+        callback();
+    })
+}
+
+
+function prodServerBuild(callback){
+    const prodServerWebpack = webpack(createServerConfig(true));
+    prodServerWebpack.run((error, stats) => {
+        outputWebpack("Prod:Server", error, stats);
+        callback();
+    });
+}
+
+function outputWebpack(callBack){
+    
+}
