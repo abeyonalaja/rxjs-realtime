b65f415328ff6d7365232668c14a13d09c201c14
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-02 01:38:19 +0100

    Add gulp tasks for production build of client and server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,11 +16,24 @@ gulp.task("clean:client", cb => rimraf("./public/build", cb));
 gulp.task("clean", gulp.parallel("clean:server", "clean:client"));
 
 gulp.task("dev:server", gulp.series("clean:server", devServerBuild));
+gulp.task("dev", gulp
+          .series(
+              "clean",devServerBuild, gulp.parallel(
+                  devServerWatch, devServerReload)))
 
 gulp.task("prod:server", gulp.series("clean:server", prodServerBuild));
+gulp.task("prod:client", gulp.series("clean:client", prodClientBuild));
+gulp.task("prod", gulp.series("clean", gulp.parallel(prodServerBuild, prodClientBuild)));
 
 // -----------------------------
 // Private Server tasks
+function prodClientBuild(callback) {
+    const compiler = webpack(createClientConfig(false));
+    compiler.run((err,stats) => {
+        outputWebpack("Prod:Client", err, stats);
+        callback();
+    })
+}
 
 
 // -----------------------------
