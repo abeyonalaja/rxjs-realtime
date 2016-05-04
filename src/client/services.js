448545a89a9feb03eb8a0942cae75909ffa0511b
Abey Onalaja <abey.onalaja@gmail.com>
2016-05-04 18:37:20 +0100

    Fix typeo in webpack-client vendorModules array

--- src/client/services.js
+++ src/client/services.js
@@ -2,3 +2,8 @@
 import io from "socket.io-client";
 
 export const socket = io({ autoConnecnt: false });
+
+// Create socket wrapper
+// Create playlist store
+// Create user store
+// Create chat store.
